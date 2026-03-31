c30b2099ff51ca28fcb912530b44c236f5afa484
Huang Yi <dssocool@qq.com>
2026-03-31 23:00:52 +0800

    Update MenuBar to disable Query button and add styling for disabled state
    - Modified the MenuBar component to disable the Query button, preventing user interaction when not applicable.
    - Added CSS styles for the disabled button state, improving visual feedback for users by indicating unavailability.

--- DataProtectionTool.ControlCenter/frontend/src/components/MenuBar.tsx
+++ DataProtectionTool.ControlCenter/frontend/src/components/MenuBar.tsx
@@ -14,7 +14,7 @@ interface MenuBarProps {
 
 export default function MenuBar({
   onSqlServerConnection,
-  onNewQuery,
+  onNewQuery: _onNewQuery,
   onViewConnections,
   onViewFlows,
   oid,
@@ -76,8 +76,8 @@ export default function MenuBar({
                     </li>
                   </ul>
                 </li>
-                <li className="menu-item">
-                  <button onClick={() => handleAction(onNewQuery)}>
+                <li className="menu-item disabled">
+                  <button disabled>
                     Query
                   </button>
                 </li>
